6641a654e720f5d8b448e0cfadf699da0275d7a4
tfkolitz <tfkolitz@gmail.com>
2020-11-26 18:14:29 +0200

    travels+ contracttouser logic
    לא גמור
    צריך להוסיף דברים ויש שגיאות

---
+++ Web_Api/BL/Logic/ContractToUserLogic.cs
@@ -0,0 +1,32 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Logic
+{
+    class ContractToUserLogic
+    {
+        //להוסיף טבחלת חוזה למשתמש וDTO
+        public static bool AddContract(contractsDTO con)
+        {
+
+            static RavKav db = new RavKav();
+            contractToUser c = Convertions.Convertion(con);
+            try
+            {
+                db.Users.Add(c);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+    }
+}
+}
--- Web_Api/BL/Logic/TravelLogic.cs
+++ Web_Api/BL/Logic/TravelLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BL.ModelDTO;
+using DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +10,24 @@ namespace BL.Logic
 {
     class TravelLogic
     {
-
-        Console.WriteLine();
-
-
-
-            
-
-    }
+        static RavKav db = new RavKav();
+        // שליפת כל הנסיעות של משתמש
+        public static List<string> GetTravelsById(int id)
+        {
+            //לבדוק איך אפשר להחזיר רשימה מסוג נסיעות
+            List<string> travelsById = new List<string>();
+            travelsById = db.Transits.Where(x => x.id == id).Select(x => x.id + x.bas + x.price + x.areaID + x.InternalOrIntermediate + x.transitTrip + x.userID + x.date).ToList();
+            return travelsById;
+        }
+        //מציאת שתי נסיעות הכי יקרות
+        public static List<string> GetExpenciveTravel(int id)
+        {
+            //לא גמור
+            //איך להוציא רק שתי נסיעות 
+            List<string> expenciveTravel = new List<string>();
+            expenciveTravel = db.Transits.Where(x => x.id == id).Select(x => x.id + x.bas + x.price + x.areaID + x.InternalOrIntermediate + x.transitTrip + x.userID + x.date).ToList();
+            return expenciveTravel;
+        }
+       
+       
 }
